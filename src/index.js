07647065c359d6395d96b938c21582e84c3af4ae
Sameer Zafar <venomfire2525@gmail.com>
2024-07-14 14:03:09 +0500

    utils folder setup

--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is running at port: "+process.env.PORT);
+    });
+})
+.catch((err)=>{console.log("Mongo DB Error: "+err)})
 
